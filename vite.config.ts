cc74cbcbc090b8890a91a9f3bc9b409a9231a38c
Sufi Gaffar <sufi@zeroheight.com>
2026-05-20 21:05:41 +0100

    add deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/words-daily/',
   plugins: [react()],
   server: {
     allowedHosts: ['0b0b-209-35-68-167.ngrok-free.app'],
